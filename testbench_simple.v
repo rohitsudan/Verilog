495e75e365896221f5b8c1adbd853561bd2ed482
rohitsudan <rohitcoder94@gmail.com>
2016-09-15 07:54:59 +0530

    bus registers included and working

--- testbench_simple.v
+++ testbench_simple.v
@@ -1,23 +1,25 @@
 `timescale 1ns/10ps
 module testbench_simple;
-reg a,b,s;
-wire out;
+parameter width = 8;
+reg [width-1:0] a,b;
+reg s;
+wire [width-1:0] out;
 mux2to1 mymux(a,b,s,out);
 initial
 begin
 	$dumpfile("testbench_simple.vcd");//the name of the .vcd file should be as the same as the testbecnh file
 	$dumpvars(0,testbench_simple);
-	a=0;
-	b=0;
+	a=78;//this is in decimal
+	b='b00000010;//this is in binary
 	s=0;
 	#1;
-	a=1;
+	a=150;
 	#1;
 	s=1;
 	b=1;
 	#1;
 	s=0;
-	a=0;
+	a=200;
         $finish;
 end
 endmodule
