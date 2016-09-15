7d68df56598eaaaf521fb6e1e19cc88a5fa3f4b2
rohitsudan <rohitcoder94@gmail.com>
2016-09-15 07:36:41 +0530

    Behaviuoral code completed and working code

--- simple.v
+++ simple.v
@@ -1,13 +1,28 @@
-module 2to1mux(in0,in1,s,out);
+module mux2to1(in0,in1,s,out);
 
 input in0,in1,s;
 output out;
-
+reg out;
+// this design is using gate level or structural level
+/*
 not inv1(s_bar,s);
 and and1(a1,s_bar,in0);
 and and2(a2,s,in1);
 or or1(out,a1,a2);
+*/
+// this design is using behavioural level
 
+always @ (in0 or in1 or s)
+begin
+    if(s == 0)
+    begin
+    	out = in0;
+    end
+    else
+    begin
+	out = in1;
+    end
+end 
 
 endmodule
 
--- testbench_simple.v
+++ testbench_simple.v
@@ -1,4 +1,25 @@
 `timescale 1ns/10ps
-module testbench_simple();
+module testbench_simple;
 reg a,b,s;
+wire out;
+mux2to1 mymux(a,b,s,out);
+initial
+begin
+	$dumpfile("testbench_simple.vcd");//the name of the .vcd file should be as the same as the testbecnh file
+	$dumpvars(0,testbench_simple);
+	a=0;
+	b=0;
+	s=0;
+	#1;
+	a=1;
+	#1;
+	s=1;
+	b=1;
+	#1;
+	s=0;
+	a=0;
+        $finish;
+end
+endmodule
+
 
